9963c3c322c7f4cc5d2dca2380ef780e2ebe6242
helaluddin100 <freelancerhelaluddin@gmail.com>
2022-09-19 14:36:24 +0600

    model fix

--- src/component/gallery.js
+++ src/component/gallery.js
@@ -605,6 +605,14 @@ function Gallery() {
       >
         <div class='modal-dialog modal-dialog-centered modal-xl'>
           <div class='modal-content'>
+            <div class='modal-header'>
+              <button
+                type='button'
+                class='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
             <div class='modal-body'>
               <div className='row'>
                 <div className='col-lg-6 mb-4'>
